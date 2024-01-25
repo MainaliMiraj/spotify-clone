84a128bd2780b05ced7c7a7d0d64e66635ab1540
Miraj Mainali <merazzmainali05@gmail.com>
2024-01-25 21:18:47 +1100

    added the SidebarItem and link for the routing

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -31,8 +32,20 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   return (
     <div className="flex h-full">
       <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
-        <Box>SideBar Navigation</Box>
-        <Box className="overflow-y-auto h-full">Song Library</Box>
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem
+                key={item.label}
+                href={item.href}
+                active={item.active}
+                label={item.label}
+                icon={item.icon}
+              />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-yy-auto h-full">Song Library</Box>
       </div>
     </div>
   );
