f7cb8a5908bf2bb8b009726d9e24af2ec20a99c4
Miraj Mainali <merazzmainali05@gmail.com>
2024-01-26 21:10:17 +1100

    added the custom button component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -35,6 +38,23 @@ const Header: React.FC<HeaderProps> = ({ className, children }) => {
           >
             <RxCaretRight size={35} className="text-white" />
           </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-70 transition ">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-70 transition ">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+            <>
+            <div>
+                <Button>
+                    Sign up
+                </Button>
+            </div>
+            </>
         </div>
       </div>
     </div>
