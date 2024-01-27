f48945fc5a1c7c5f3d65d7946cd0154b947b2dd8
Miraj Mainali <merazzmainali05@gmail.com>
2024-01-27 20:52:04 +1100

    added the types and useUser hook for the providers for authentication and supabase

---
+++ hooks/useUser.tsx
@@ -0,0 +1,33 @@
+import { userDetails } from "@/types";
+import { User } from "@supabase/auth-helpers-nextjs";
+import {
+  useSessionContext,
+  useUser as useSupaUser,
+} from "@supabase/auth-helpers-react";
+import { createContext } from "react";
+import { Subscription } from "react-redux";
+
+type UserContextType = {
+  accessToken: string | null;
+  user: User | null;
+  userDetails: userDetails | null;
+  isLoading: boolean;
+  subscription: Subscription | null;
+};
+
+export const UserContext = createContext<UserContextType | undefined>(
+  undefined
+);
+
+export interface Props {
+  [propName: string]: any;
+}
+
+export const MyUserContextProvider = (props: Props) => {
+  const {
+    session,
+    isLoading: isLoadingUser,
+    supabaseClient: supabase,
+  } = useSessionContext();
+  const user = useSupaUser();
+};
