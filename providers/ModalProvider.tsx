9e91ffd0a158da10cab2f234eb24a3c4c32bdcde
Miraj Mainali <merazzmainali05@gmail.com>
2024-02-01 08:53:49 +1100

    added the AuthModal and the useAuthModal to components for authentication login/signup

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import Modal from "@/components/Modal";
+import AuthModal from "@/components/AuthModal";
+
 import { useEffect, useState } from "react";
 
 const ModalProvider = () => {
@@ -11,7 +12,7 @@ const ModalProvider = () => {
   if (!isMounted) return null;
   return (
     <>
-      <Modal title="Modal test" description="Description Test" isOpen onChange={()=>{}}>Test Children</Modal>
+      <AuthModal />
     </>
   );
 };
