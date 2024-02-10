bb8949fdd489477340773917a19c02802e48fb0a
Miraj Mainali <merazzmainali05@gmail.com>
2024-02-11 08:44:59 +1100

    added the get song component list to fetch the song from the server

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
-import { rejects } from "assert";
-import { resolve } from "path";
+
+export const revalidate=0;
 
 export default function Home() {
   return (
