99d66c8fa7584332385e313f91640d7517bbdb38
Miraj Mainali <merazzmainali05@gmail.com>
2024-02-15 06:40:09 +1100

    added the searchContent compoent in the search page to search for the desired songs

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,5 +1,35 @@
+"use client";
+
+import qs from "query-string";
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+
+import { useState, useEffect } from "react";
+import Input from "./Input";
+
 const SearchInput = () => {
-  return <input type="text" className="p-2 max-w-sm rounded-sm"></input>;
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  useEffect(() => {
+    const query = {
+      title: debouncedValue,
+    };
+    const url = qs.stringifyUrl({
+      url: "/search",
+      query: query,
+    });
+    router.push(url);
+  }, [debouncedValue, router]);
+
+  return (
+    <Input
+      placeholder="What do you want to listen today?"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
 };
 
 export default SearchInput;
