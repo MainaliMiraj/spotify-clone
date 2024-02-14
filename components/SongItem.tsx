7cad3535d0a45ffaff0fa571b23c5080edacc8cc
Miraj Mainali <merazzmainali05@gmail.com>
2024-02-14 21:18:53 +1100

    created the play button component and added it to the page

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -3,6 +3,7 @@
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
 import Image from "next/image";
+import PlayButton from "./PlayButton";
 
 interface SongItemProps {
   data: Song;
@@ -14,11 +15,23 @@ const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
   return (
     <div
       onClick={() => onClick(data.id)}
-      className="realtive group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10  transition p-3 "
+      className="relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10  transition p-3 "
     >
       <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
-        <Image className="object-cover" src={imagePath || null} fill alt="Image" />
+        <Image
+          className="object-cover"
+          src={imagePath || "/favicon.ico"}
+          fill
+          alt="Image"
+        />
       </div>
+      <div className="flex-flex-col item-start w-full pt-4 gap-y-1">
+        <p className="font-semibold truncate w-full">{data.title}</p>
+        <p className="text-neutral-400 text-sm pb-4 w-full truncate">
+          By {data.author}
+        </p>
+      </div>
+      <div className="absolute bottom-24 right-5"><PlayButton/></div>
     </div>
   );
 };
