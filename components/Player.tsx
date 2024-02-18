5315f1d42d5e546f0c7117f7baa197c5d7a1e818
Miraj Mainali <merazzmainali05@gmail.com>
2024-02-18 12:08:45 +1100

    added the useLoadSongUrl to load the song from the server

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,12 +1,18 @@
-import usePlayer from "@/hooks/usePlayer"
-
+"use client";
+import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
+import usePlayer from "@/hooks/usePlayer";
 
 const Player = () => {
-  const player=usePlayer();
-  
-  return (
-    <div>Player</div>
-  )
-}
+  const player = usePlayer();
+  const { song } = useGetSongById(player.activeId);
 
-export default Player
+  const songUrl = useLoadSongUrl(song!);
+  if (!song || !songUrl || player.activeId) {
+    return null;
+  }
+
+  return <div>Player</div>;
+};
+
+export default Player;
