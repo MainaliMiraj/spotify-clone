a55861ada60fdc3f9aa924ef51fb351ec7986075
Miraj Mainali <merazzmainali05@gmail.com>
2024-02-24 08:08:55 +1100

    added the useOnPlay hook to play the song by the user

--- components/Player.tsx
+++ components/Player.tsx
@@ -12,7 +12,7 @@ const Player = () => {
     return null;
   }
 
-  return <div>Player</div>;
+  return <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">Player</div>;
 };
 
 export default Player;
