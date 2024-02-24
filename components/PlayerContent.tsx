222223c9284573fd0c8a80401211318afb4ed829
Miraj Mainali <merazzmainali05@gmail.com>
2024-02-25 09:21:39 +1100

    added the previos and next song functionalities

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import { useState } from "react";
 import { Song } from "@/types";
-import MediaItem from "./MediaItem";
-import LikeButton from "./LikeButton";
-
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { AiFillBackward, AiFillForward } from "react-icons/ai";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+
+import MediaItem from "./MediaItem";
+import LikeButton from "./LikeButton";
 import Slider from "./Slider";
+import usePlayer from "@/hooks/usePlayer";
 
 interface PlayerContentProps {
   song: Song;
@@ -15,8 +17,39 @@ interface PlayerContentProps {
 }
 
 const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
-  const Icon = true ? BsPauseFill : BsPlayFill;
-  const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+  const player = usePlayer();
+  const [volume, setVolume] = useState(1);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+  const onPlayNext = () => {
+    if (player.ids.length === 0) return;
+
+    const currrentIndex = player.ids.findIndex((id) => {
+      id === player.activeId;
+    });
+
+    const nextSong = player.ids[currrentIndex + 1];
+
+    if (!nextSong) {
+      return player.setId(player.ids[0]);
+    }
+    player.setId(nextSong);
+  };
+
+  const onPlayPrevious = () => {
+    if (player.ids.length === 0) return;
+
+    const currrentIndex = player.ids.findIndex((id) => id === player.activeId);
+
+    const previousSong = player.ids[currrentIndex - 1];
+
+    if (!previousSong) {
+      return player.setId(player.ids[player.ids.length - 1]);
+    }
+    player.setId(previousSong);
+  };
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
       <div className="flex w-full justify-start">
@@ -37,7 +70,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
         <AiFillBackward
           size={30}
           className="text-neutral-400 cursor-pointer hover:text-white transition"
-          onClick={() => {}}
+          onClick={onPlayPrevious}
         />
         <div
           onClick={() => {}}
@@ -46,7 +79,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
           <Icon className="text-black" size={30} />
         </div>
         <AiFillForward
-          onClick={() => {}}
+          onClick={onPlayNext}
           size={30}
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
@@ -58,7 +91,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
             onClick={() => {}}
             className="cursor-pointer "
           />
-          <Slider/>
+          <Slider />
         </div>
       </div>
     </div>
