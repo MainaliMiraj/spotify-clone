0699ad2566e27f30a79d38f7913ebc92629deb46
Miraj Mainali <merazzmainali05@gmail.com>
2024-02-25 09:09:24 +1100

    completed the slider componenet

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,30 +1,54 @@
 "use client";
 
-import React from "react";
 import * as RadixSlider from "@radix-ui/react-slider";
 
-interface SliderProps {
+interface SlideProps {
   value?: number;
   onChange?: (value: number) => void;
 }
-const Slider: React.FC<SliderProps> = ({ value = 1, onChange }) => {
+
+const Slider: React.FC<SlideProps> = ({ value = 1, onChange }) => {
   const handleChange = (newValue: number[]) => {
     onChange?.(newValue[0]);
   };
+
   return (
-    <div>
-      <RadixSlider.Root
-        className="relative flex items-center select-none touch-none w-fill h-10"
-        defaultValue={[1]}
-        value={[value]}
-        onValueChange={handleChange}
-        max={1}
-        step={0.1}
-        aria-label="Volume"
+    <RadixSlider.Root
+      className="
+        relative 
+        flex 
+        items-center 
+        select-none 
+        touch-none 
+        w-full 
+        h-10
+      "
+      defaultValue={[1]}
+      value={[value]}
+      onValueChange={handleChange}
+      max={1}
+      step={0.1}
+      aria-label="Volume"
+    >
+      <RadixSlider.Track
+        className="
+          bg-neutral-600 
+          relative 
+          grow 
+          rounded-full 
+          h-[3px]
+        "
       >
-        <RadixSlider.Track></RadixSlider.Track>
-      </RadixSlider.Root>
-    </div>
+        <RadixSlider.Range
+          className="
+            absolute 
+            bg-white 
+            rounded-full 
+            h-full
+          "
+        />
+      </RadixSlider.Track>
+    </RadixSlider.Root>
   );
 };
 
